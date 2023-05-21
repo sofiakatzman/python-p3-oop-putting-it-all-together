b815238be0b357010dbf6bddb316f726d5da2cfa
Sofia Katzman <katzmansof@gmail.com>
2023-05-21 00:31:58 -0400

    finished lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+
+    def set_title(self, title):
+        self._title = title
+
+    def get_title(self):
+        return self._title
+
+    title = property(get_title, set_title)
+
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def get_page_count(self):
+        return self._page_count
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size 
+    
+    def set_brand(self, brand):
+        self._brand = brand
+
+    def get_brand(self):
+        return self._brand
+    
+    brand = property(get_brand, set_brand)
+
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size 
+        else: 
+            print("size must be an integer")
+    def get_size(self):
+        return self._size
+    
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
